e7bbbc8966395602744abcb0eb974f2d0adf8650
Norfirion <Norfirionszt@gmail.com>
2021-04-07 13:58:33 +0200

    Kezdet a csoportmunkához

---
+++ public_html/javascript.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", init);
+
+
+/* segédfüggvény*/
+function ID(nev){
+    return document.getElementById(nev);
+}
+function $(nev){
+   return document.querySelectorAll(nev);
+}
