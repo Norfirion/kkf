0c1b1bc18db59f3b3212e2477362848a553ada38
T. Virág <virmoc411@gmail.com>
2021-04-07 16:09:13 +0200

    5.feladat

--- public_html/javascript.js
+++ public_html/javascript.js
@@ -8,3 +8,15 @@ function ID(nev){
 function $(nev){
    return document.querySelectorAll(nev);
 }
+function init(){
+    for (var i = 0; i < ("aside img").length; i++) {
+        $("aside img")[0].addEventListener("mouseover", feladat5);
+        $("aside img")[0].addEventListener("mouseout", feladat6);
+}
+}
+function feladat5(){
+    this.classList.add("kiemel");
+}
+function feladat6(){
+    this.classList.remove("kiemel");
+}
