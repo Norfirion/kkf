dd776d5785a624bbfb7f4de8864cb9e52c6429ee
IncMilan <01milan28@gmail.com>
2021-04-07 14:50:57 +0200

    Javafeladat2

--- public_html/javascript.js
+++ public_html/javascript.js
@@ -8,3 +8,11 @@ function ID(nev){
 function $(nev){
    return document.querySelectorAll(nev);
 }
+function keszito(){
+    document.getElementById("keszitok").innerHTML="Szabó Milán, Szabó Tamás, Szántai barna, Tóth Virág";
+    
+}
+
+function init(){
+    keszito();
+}
